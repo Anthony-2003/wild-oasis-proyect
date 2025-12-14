63a8374520fea4bd3cfdfdc6770cf1294b9dc644
Anthony Volquez <anthony1volquez@gmail.com>
2025-12-14 13:29:23 -0400

    react supabase url and supabase key by .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabaseUrl = 'https://qkpxunvgmdsijnhhbean.supabase.co/'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcHh1bnZnbWRzaWpuaGhiZWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNjIyMDcsImV4cCI6MjAzODczODIwN30.d3I94oEPgh9qUhIp3051EqqLPJOKB5aXrD4E5ErkA-8";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const supabase2 = createClient(supabaseUrl, supabaseKey, {

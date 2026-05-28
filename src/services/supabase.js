6d7e011c8f63a6030576cbc03976992b9d15a7dd
anthono2003 <anthony1volquez@gmail.com>
2026-05-28 12:20:19 -0400

    fix: add new supabase credentials

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabaseUrl = 'https://qkpxunvgmdsijnhhbean.supabase.co/'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcHh1bnZnbWRzaWpuaGhiZWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNjIyMDcsImV4cCI6MjAzODczODIwN30.d3I94oEPgh9qUhIp3051EqqLPJOKB5aXrD4E5ErkA-8";
+export const supabaseUrl = 'https://kpxvqgxjmicjpojyfwgy.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtweHZxZ3hqbWljanBvanlmd2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MzA0MzksImV4cCI6MjA4MTMwNjQzOX0.GBa8FW0Ona7Sr8PTHirloKJJmGuFDzeUrhwPZvnq43E";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const supabase2 = createClient(supabaseUrl, supabaseKey, {
